25ae84602d7c0851ff7c213491605fb85352fa21
MystiqueMide <splashmediahub@gmail.com>
2026-08-12 20:33:32 +0100

    feat(proof): centralize live Flare demo evidence

---
+++ site/lib/live-proof.ts
@@ -0,0 +1,19 @@
+export const liveProof = {
+  network: "Flare Coston2",
+  chainId: 114,
+  campaign: "0x5e77dfD9c2142B7e9e7A11017b0B5417EC5A9cc6",
+  instructionTx: "0x8142d704296efd6d9e6dd87a6aac1e3ce1abb5c4d643422d524b3d86eac02d47",
+  instructionId: "0x315b46e12fe1dcce3387155bcb69c8b321bc3c082875ce6101b4e9e09504a052",
+  settlementTx: "0xf8269c7aab0ad00ed8695cc07d6defb7d5f019b58068b0ddfc1cf283d74fc4a6",
+  teeSigner: "0x9103b8400FAae0a243508F577398CD9FbfbEb5fd",
+  paidAmount: "20",
+  remainingEscrow: "80",
+  replayRejected: true,
+  simulatedTee: true,
+  instructionUrl:
+    "https://coston2-explorer.flare.network/tx/0x8142d704296efd6d9e6dd87a6aac1e3ce1abb5c4d643422d524b3d86eac02d47",
+  settlementUrl:
+    "https://coston2-explorer.flare.network/tx/0xf8269c7aab0ad00ed8695cc07d6defb7d5f019b58068b0ddfc1cf283d74fc4a6",
+  campaignUrl:
+    "https://coston2-explorer.flare.network/address/0x5e77dfD9c2142B7e9e7A11017b0B5417EC5A9cc6",
+} as const;
